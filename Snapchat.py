bf4d40a5f2e3705d79f1c4e4f43379c655250451
UsmannK <usmann96@gmail.com>
2014-10-28 21:30:37 -0400

    Added Snap Saving capability
    First Functioning Commit

--- Snapchat.py
+++ Snapchat.py
@@ -3,62 +3,86 @@ import hashlib
 import json
 import requests
 import time
+import base64
+from Crypto.Cipher import AES
 
-URL_BASE = "https://feelinsonice.appspot.com"
-STATIC_TOKEN = "m198sOkJEn37DjqZ32lpRu76xmw288xSQ9"
-ENCRYPT_KEY_2 = "M02cnQ51Ji97vwT4"
-REQ_TOKEN_PATTERN = "0001110111101110001111010101111011010001001110011000110001000110"
-REQ_TOKEN_SECRET = "iEk21fuwZApXlz93750dmW22pw389dPwOk"
-USERNAME = ""
+class Snapchat:
+	URL_BASE = "https://feelinsonice.appspot.com"
+	STATIC_TOKEN = "m198sOkJEn37DjqZ32lpRu76xmw288xSQ9"
+	ENCRYPT_KEY_2 = "M02cnQ51Ji97vwT4"
+	REQ_TOKEN_PATTERN = "0001110111101110001111010101111011010001001110011000110001000110"
+	REQ_TOKEN_SECRET = "iEk21fuwZApXlz93750dmW22pw389dPwOk"
+	USERNAME = ""
+	AUTH_TOKEN = ""
+	unpad = lambda self, s : s[0:-ord(s[-1])]
 
-def request_token(auth_token, timestamp):
-    first = hashlib.sha256(REQ_TOKEN_SECRET + auth_token).hexdigest()
-    second = hashlib.sha256(str(timestamp) + REQ_TOKEN_SECRET).hexdigest()
-    bits = [first[i] if c == "0" else second[i] for i, c in enumerate(REQ_TOKEN_PATTERN)]
-    return "".join(bits)
+	def request_token(self, auth_token, timestamp):
+	    first = hashlib.sha256(self.REQ_TOKEN_SECRET + auth_token).hexdigest()
+	    second = hashlib.sha256(str(timestamp) + self.REQ_TOKEN_SECRET).hexdigest()
+	    bits = [first[i] if c == "0" else second[i] for i, c in enumerate(self.REQ_TOKEN_PATTERN)]
+	    return "".join(bits)
 
-def get_time():
-	return str(time.time())[:10]
+	def get_time(self):
+		return str(time.time())[:10]
 
-def login(username, password):
-	global USERNAME
-	USERNAME = username
-	time = get_time()
-	req_token = request_token(STATIC_TOKEN, time)
-	r = requests.post(URL_BASE + "/bq/login", data={
-    "req_token": req_token,
-    "timestamp": time,
-    "username": username,
-    "password": password
-	}, headers={"User-agent": None})
-	auth_token, username = r.json()["auth_token"], r.json()["username"]
-	return auth_token
+	def login(self, username, password):
+		self.USERNAME = username
+		time = self.get_time()
+		req_token = self.request_token(self.STATIC_TOKEN, time)
+		r = requests.post(self.URL_BASE + "/bq/login", data={
+	    "req_token": req_token,
+	    "timestamp": time,
+	    "username": username,
+	    "password": password
+		}, headers={"User-agent": None})
+		auth_token, username = r.json()["auth_token"], r.json()["username"]
+		self.AUTH_TOKEN = auth_token
+		return 1 if len(self.AUTH_TOKEN) != 0 else 0
 
-def get_snap_list(auth_token):
-	time = get_time()
-	req_token = request_token(auth_token, time)
-	r = requests.post(URL_BASE + "/bq/updates", data={
-    "req_token": req_token,
-    "timestamp": time,
-    "username": USERNAME,
-	}, headers={"User-agent": None})
-	unread_snaps = []
-	for jsonObject in r.json()["snaps"]:
-		if(jsonObject["st"] == 1 and jsonObject["id"].endswith("r")):
-			unread_snaps.append(jsonObject["id"] + " img" if jsonObject["m"] == 0 else " vid")
-	return unread_snaps
+	def get_snap_list(self):
+		time = self.get_time()
+		req_token = self.request_token(self.AUTH_TOKEN, time)
+		r = requests.post(self.URL_BASE + "/bq/updates", data={
+	    "req_token": req_token,
+	    "timestamp": time,
+	    "username": self.USERNAME,
+		}, headers={"User-agent": None})
+		unread_snaps = []
+		for jsonObject in r.json()["snaps"]:
+			if(jsonObject["st"] == 1):
+				unread_snaps.append(jsonObject)
+		return unread_snaps
 
-#def get_snaps(unread_snaps):
+	def decrypt(self, data):
+		if len(self.ENCRYPT_KEY_2) not in (16, 24, 32):
+			raise ValueError("Key must be 16, 24, or 32 bytes")
+		cipher = AES.new(self.ENCRYPT_KEY_2, AES.MODE_ECB)
+		return cipher.decrypt(data)
+
+	def save_snaps(self, snap_id):
+		if(snap_id["id"].endswith("s")):
+			return
+		time = self.get_time()
+		req_token = self.request_token(self.AUTH_TOKEN, time)
+		r = requests.post(self.URL_BASE + "/ph/blob", data={
+	    "req_token": req_token,
+	    "timestamp": time,
+	    "username": self.USERNAME,
+	    "id": snap_id["id"],
+		}, headers={"User-agent": None})
+		image = r.content
+		image_decrypted = s.decrypt(image)
+		f = open(snap_id["id"] + (".jpg" if snap_id["m"] == 0 else ".mp4"), 'wb')
+		f.write(image_decrypted)
+		f.close()
 
 
 
-auth_token1 = login("UsmannK", "R6t40xxla3!")
-unread_snaps =  get_snap_list(auth_token1)
-for snap in unread_snaps:
-	print snap
-#print json.dumps(get_snap_list(), sort_keys=True, indent=4, separators=(',', ': '))
-
-
+# s = Snapchat()
+# auth_token1 = s.login("USERNAME", "PASSWORD")
+# unread_snaps =  s.get_snap_list()
+# for snap in unread_snaps:
+# 	s.save_snaps(snap) # Saves snaps in current directory with IDs as filenames
 
 
 
